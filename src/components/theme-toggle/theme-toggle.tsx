4e9e51653c3b3a6ba547e5ab763ec5e7b7e7fe62
Pasha <pasha@getinkworks.com>
2024-06-06 13:49:15 -0700

    fixed toggle clicking issue

--- src/components/theme-toggle/theme-toggle.tsx
+++ src/components/theme-toggle/theme-toggle.tsx
@@ -1,10 +1,10 @@
 import { MouseEventHandler, useEffect, useRef } from 'react';
-import './theme-toggle.css';
 import { getLsItem, setLsItem } from '../../utils/helper-utils';
+import './theme-toggle.css';
 
 const setAriaLabel = (target: HTMLInputElement) => {
-	const themeToggleParent = target.parentElement?.parentElement;
-	themeToggleParent?.setAttribute('aria-label', `Switch to ${target.checked ? 'light' : 'dark'} theme`);
+	const themeToggleParent = target.parentElement;
+	themeToggleParent?.setAttribute('aria-label', `Switch to ${target.checked ? 'light' : 'dark'} mode`);
 };
 
 export default function ThemeToggle() {
@@ -31,18 +31,16 @@ export default function ThemeToggle() {
 	};
 
 	return (
-		<div id="themeToggle" className="theme-toggle">
-			<label>
-				<input ref={inputRef} type="checkbox" onClick={handleThemeToggle} />
-				<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 472.39 472.39">
-					<g className="toggle-sun">
-						<path d="M403.21,167V69.18H305.38L236.2,0,167,69.18H69.18V167L0,236.2l69.18,69.18v97.83H167l69.18,69.18,69.18-69.18h97.83V305.38l69.18-69.18Zm-167,198.17a129,129,0,1,1,129-129A129,129,0,0,1,236.2,365.19Z" />
-					</g>
-					<g className="toggle-circle">
-						<circle className="cls-1" cx="236.2" cy="236.2" r="103.78" />
-					</g>
-				</svg>
-			</label>
-		</div>
+		<label id="themeToggle" className="theme-toggle">
+			<input ref={inputRef} type="checkbox" onClick={handleThemeToggle} />
+			<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 472.39 472.39">
+				<g className="toggle-sun">
+					<path d="M403.21,167V69.18H305.38L236.2,0,167,69.18H69.18V167L0,236.2l69.18,69.18v97.83H167l69.18,69.18,69.18-69.18h97.83V305.38l69.18-69.18Zm-167,198.17a129,129,0,1,1,129-129A129,129,0,0,1,236.2,365.19Z" />
+				</g>
+				<g className="toggle-circle">
+					<circle className="cls-1" cx="236.2" cy="236.2" r="103.78" />
+				</g>
+			</svg>
+		</label>
 	);
 }
