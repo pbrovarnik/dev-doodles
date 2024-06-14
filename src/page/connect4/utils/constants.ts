a33b96f322484e29986d5d4fb09f4de3e402564a
Pasha <pasha@getinkworks.com>
2024-06-13 22:12:39 -0700

    renaming page to pages

--- src/page/connect4/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-export const RED = 'Red';
-export const YELLOW = 'Yellow';
-export const TIE = 'Tie';
-export const RED_COLOR = '#d23d32';
-export const YELLOW_COLOR = '#fae04c';
-export const TIE_COLOR = '#b4762f';
--- src/page/home/home.tsx
+++
@@ -1,8 +0,0 @@
-export default function index() {
-	return (
-		<div>
-			<h1>Welcome to the world of DevDoodles!</h1>
-			<p>(Page is under construction)</p>
-		</div>
-	);
-}
