73a1d15c47e7e512119e59f306bdb9b2591da2bf
Pasha <pasha@getinkworks.com>
2024-06-18 19:12:10 -0700

    adding https

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,7 @@
 import useFetch from '../../hooks/useFetch';
 
 export default function Home() {
-	const data = useFetch({ defaultData: '', url: 'http://pashginx.onrender.com/api/v1' });
+	const data = useFetch({ defaultData: '', url: 'https://pashginx.onrender.com/api/v1' });
 	console.log('data', data);
 	return (
 		<div>
