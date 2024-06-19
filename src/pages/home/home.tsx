e307d74f380da1ec14c52ab4136224a9846f470a
Pasha <pasha@getinkworks.com>
2024-06-19 00:25:50 -0700

    removing fetch for testing pashginx

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,4 @@
-import useFetch from '../../hooks/useFetch';
-
 export default function Home() {
-	const data = useFetch({ defaultData: '', url: 'https://pashginx.onrender.com/api/v1' });
-	console.log('data', data);
 	return (
 		<div>
 			<h1>Welcome to the world of DevDoodles!</h1>
