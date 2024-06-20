40f44eddbe1e516f0f60e2b1ab20a676cffe78aa
Pasha <pasha@getinkworks.com>
2024-06-20 16:09:26 -0700

    removing slash from base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	base: '/dev-doodles/',
+	base: '/dev-doodles',
 	envPrefix: 'APP_',
 	plugins: [react()],
 });
